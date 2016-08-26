d92a85707ce967c55b2eb76ddca747c8bf360a0c
kalmas <kylealmas@gmail.com>
2016-08-25 20:51:03 -0400

    remove broken search X

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -14,6 +14,7 @@ import '../../public/css/styles.css';
 })
 export class AppComponent
 {
+    public query: string = '';
     private queryStream: Subject<string> = new Subject<string>();
     private songService: SongService;
     private songs: Observable<Song[]>;
